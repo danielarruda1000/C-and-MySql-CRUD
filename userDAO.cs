5991c52bf768bd07515860ae35675f75d8b5f1e6
Daniel Arruda <danielarruda1000>
2019-10-12 19:54:43 -0300

    Insert and Select all

---
+++ userDAO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace dataBaseCRUD
+{
+    public class userDAO 
+    {
+
+        private MySqlConnection connection;
+
+        private string dbWay = "SERVER=localhost;DATABASE=bank;UID=root;PASSWORD=root;";
+      
+        public void insertUser(userModel u)
+        {
+            try
+            {
+                connection = new MySqlConnection(dbWay);
+                connection.Open();
+
+                string insert = "INSERT INTO users(userName,id,saldo) VALUES('" + u.Name + "','" + u.Id + "','" + u.Balance + "')";
+
+                MySqlCommand cmd = new MySqlCommand(insert, connection);
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public List<userModel> loadAll()
+        {
+            List<userModel> users = new List<userModel>();
+
+            connection = new MySqlConnection(dbWay);
+            connection.Open();
+            string query = "SELECT * FROM users";
+
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+                    
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = Convert.ToInt32(reader["id"]);
+                string username = reader["userName"].ToString();
+                double balance = Convert.ToDouble(reader["saldo"]);
+
+                userModel currentUser = new userModel(username,id,balance);
+
+                users.Add(currentUser);
+            }
+
+            connection.Close();
+
+            return users;
+        }
+
+    }
+}
---
+++ userModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataBaseCRUD
+{
+    public class userModel {
+    
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Balance { get; set; }
+    
+        public userModel() { }
+        public userModel(string aName, int aId, double aBalance):this() 
+        {
+            this.Name = aName;
+            this.Id = aId;
+            this.Balance = aBalance;
+        }
+
+   
+    }
+}
